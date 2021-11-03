e5357bc8aa58bf68c1c6fbecfec115b1d402cefb
Justis Lamanna <lamanna.justis@gmail.com>
2021-11-03 12:12:50 -0500

    Tick Message

--- src/core.rs
+++ src/core.rs
@@ -1,24 +1,90 @@
+use std::collections::HashMap;
 use std::process::exit;
+use std::time::{Duration, Instant};
+
 use actix::prelude::*;
 
-pub struct Core;
+#[derive(Debug)]
+pub struct Core {
+    start_time: Instant,
+    tick: u128,
+    tick_hooks: HashMap<String, Recipient<TickEvent>>
+}
+
+impl Default for Core {
+    fn default() -> Self {
+        Self {
+            start_time: Instant::now(),
+            tick: 0,
+            tick_hooks: HashMap::new()
+        }
+    }
+}
 
 impl Actor for Core {
     type Context = Context<Self>;
+
+    fn started(&mut self, ctx: &mut Self::Context) {
+        println!("Starting Core");
+        ctx.run_interval(Duration::from_millis(8), move |act, _| {
+            let runtime = act.start_time.elapsed();
+            let ticks = (runtime.as_millis() * 6) / 100;
+            if ticks != act.tick {
+                act.tick = ticks;
+                act.tick_hooks.retain(|id, hook| {
+                    match hook.do_send(TickEvent(ticks)) {
+                        Err(SendError::Closed(_)) => {
+                            println!("{} was closed. Queueing for removal...", id);
+                            false
+                        }
+                        _ => true
+                    }
+                });
+            }
+        });
+    }
 }
 
 // Core Events
-
-//Shutdown Event
 #[derive(Message)]
 #[rtype(result = "()")]
 pub struct ShutdownEvent();
 
+#[derive(Message)]
+#[rtype(result = "()")]
+pub struct TickEvent(pub u128);
+
+#[derive(Message)]
+#[rtype(result = "()")]
+pub struct RegisterTickHook(pub String, pub Recipient<TickEvent>);
+
+#[derive(Message)]
+#[rtype(result = "()")]
+pub struct DeregisterTickHook(pub String);
+
 impl Handler<ShutdownEvent> for Core {
     type Result = ();
 
-    fn handle(&mut self, msg: ShutdownEvent, ctx: &mut Self::Context) -> Self::Result {
+    fn handle(&mut self, _: ShutdownEvent, _: &mut Self::Context) -> Self::Result {
         System::current().stop();
         exit(0);
+    }
+}
+
+impl Handler<RegisterTickHook> for Core {
+    type Result = ();
+
+    fn handle(&mut self, msg: RegisterTickHook, _: &mut Self::Context) -> Self::Result {
+        println!("Registered tick hook {}", msg.0);
+        self.tick_hooks.insert(msg.0, msg.1);
+    }
+}
+
+impl Handler<DeregisterTickHook> for Core {
+    type Result = ();
+
+    fn handle(&mut self, msg: DeregisterTickHook, _: &mut Self::Context) -> Self::Result {
+        println!("Deregistered tick hook {}", msg.0);
+        self.tick_hooks.remove(msg.0.as_str());
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,30 @@
-use std::thread;
-use std::time::Duration;
-use actix::Actor;
-use crate::core::{Core, ShutdownEvent};
+use actix::{Actor, Arbiter, Context, Handler};
+
+use crate::core::{Core, RegisterTickHook, TickEvent};
 
 mod core;
 
 #[actix_rt::main]
 async fn main() {
-    let core = Core.start();
-    thread::sleep(Duration::from_secs(5));
+    let core_arbiter = Arbiter::new();
+    let core = Actor::start_in_arbiter(&core_arbiter, |_| Core::default());
+    let test = Actor::start_in_arbiter(&core_arbiter, |_| TestPingActor::default());
 
-    let res = core.send(ShutdownEvent()).await;
+    core.do_send(RegisterTickHook(String::from("tickprint"), test.recipient()));
 
-    loop{}
+    loop {}
+}
+
+#[derive(Default)]
+struct TestPingActor;
+
+impl Actor for TestPingActor {
+    type Context = Context<Self>;
+}
+impl Handler<TickEvent> for TestPingActor {
+    type Result = ();
+
+    fn handle(&mut self, msg: TickEvent, _: &mut Self::Context) -> Self::Result {
+        println!("{}", msg.0);
+    }
 }
