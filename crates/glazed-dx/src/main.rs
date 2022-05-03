5dff350ce325780f8dc14c7ac9372da236028bd4
Justis Lamanna <lamanna.justis@gmail.com>
2022-05-03 16:30:15 -0500

    Test text render (this is gonna suck...)

--- crates/glazed-dx/src/main.rs
+++ crates/glazed-dx/src/main.rs
@@ -65,11 +65,57 @@ pub(crate) enum GameState {
     ProfessorLecture
 }
 
-fn setup(mut commands: Commands) {
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     // Spawns the camera
     commands
         .spawn_bundle(OrthographicCameraBundle::new_2d())
         .insert(Transform::from_xyz(0f32, 0f32, 1000f32));
+
+    commands
+        .spawn_bundle(UiCameraBundle::default());
+
+    commands
+        .spawn_bundle(NodeBundle {
+            style: Style {
+                size: Size::new(Val::Percent(100.0), Val::Percent(100.0)),
+                ..default()
+            },
+            color: Color::NONE.into(),
+            ..default()
+        })
+        .with_children(|p| {
+            p.spawn_bundle(NodeBundle {
+                style: Style {
+                    size: Size::new(Val::Px(256.0), Val::Px(48.0)),
+                    ..Default::default()
+                },
+                color: Color::rgba(0.25, 0.25, 0.25, 0.75).into(),
+                ..Default::default()
+            }).with_children(|p| {
+                   p.spawn_bundle(TextBundle {
+                       style: Style {
+                           margin: Rect {
+                               left: Val::Px(8.0),
+                               right: Val::Px(16.0),
+                               top: Val::Px(8.0),
+                               bottom: Val::Px(8.0)
+                           },
+                           max_size: Size::new(Val::Px(232.0), Val::Px(32.0)),
+                           ..Default::default()
+                       },
+                       text: Text::with_section(
+                           "Hello, World. This is a really really long message for you to render. Sorry.",
+                           TextStyle {
+                               font: asset_server.load("fonts/RobotoMono-Regular.ttf"),
+                               font_size: 16.0,
+                               color: Color::WHITE
+                           },
+                           Default::default()
+                       ),
+                       ..Default::default()
+                   });
+                });
+            });
 }
 
 fn init_load() -> Progress {
